3fb454fcab50c71110bee6cade637dac2a961166
LAPTOP-CICERO\cicero <cicero.info@gmail.com>
2018-09-04 21:13:16 -0300

    cicleci test

---
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -0,0 +1,32 @@
+package guru.springframework.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 
+ * @author cicero
+ *
+ */
+@Slf4j
+@Controller
+public class IndexController {
+
+	private final RecipeService recipeService;
+
+	public IndexController(RecipeService recipeService) {
+		this.recipeService = recipeService;
+	}
+
+	@RequestMapping({ "", "/", "/index" })
+	public String getIndexPage(Model model) {
+		log.debug("Index Controller initializer");
+		model.addAttribute("recipes", recipeService.getRecipes());
+
+		return "index";
+	}
+}
---
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -0,0 +1,46 @@
+package guru.springframework.domain;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author cicero
+ */
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(exclude = {"recipe"})
+@Entity
+public class Ingredient {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String description;
+	private BigDecimal amount;
+
+	@ManyToOne
+	private Recipe recipe;
+
+	@OneToOne(fetch = FetchType.EAGER)
+	private UnitOfMeasure uom;
+	
+	public Ingredient(String description, BigDecimal amount, UnitOfMeasure unitOfMeasure, Recipe recipe) {
+		this.description = description;
+		this.amount = amount;
+		this.uom = unitOfMeasure;
+		this.recipe = recipe;
+	}
+
+}
