90b900c15e1dbbd9a9dc9b561eac387e03243ead
LAPTOP-CICERO\cicero <cicero.info@gmail.com>
2018-09-14 20:58:22 -0300

    ...

--- src/main/java/guru/springframework/services/RecipeService.java
+++ src/main/java/guru/springframework/services/RecipeService.java
@@ -8,5 +8,6 @@ import guru.springframework.domain.Recipe;
 public interface RecipeService {
 	Set<Recipe> getRecipes();
 	Recipe findById(Long id);
+	RecipeCommand findCommandById(Long l);
 	RecipeCommand saveRecipeCommand(RecipeCommand command);
 }
